47d8ad17d892a4c88f9d45e7f287cfe6ca869f90
Aishwarya-1610 <88973585+Aishwarya-1610@users.noreply.github.com>
2022-04-12 16:10:28 +0530

    Create 32-The Rating Dilemma.py

---
+++ 32-The Rating Dilemma.py
@@ -0,0 +1,13 @@
+"""
+Chef really likes to compete on Codechef, and he has gained an impressive rating of X, where X>0. There is also a parallel universe, where ratings on Codechef are negative instead. The only thing that remains unchanged in the parallel universe is Chef's love for competing on Codechef. Chef's rating on Codechef in the parallel universe is Y, where Y<0.
+
+Due to some cosmic event, the parallel universe has been destroyed, resulting in Chef forgetting both X and Y. He only remembers the sum S=X+Y. He wonders what the maximum possible product of his ratings is, given that he knows the sum of his ratings.
+
+"""
+t = int(input())
+for i in range(t):
+    s = int(input())
+    y = -1
+    x = s+1 
+    print(x*y)
+    
